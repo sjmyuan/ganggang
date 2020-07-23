e9014d794d7631f16abadc8941a85622bea06f89
Jiaming Shang <shangjiaming.yuan@gmail.com>
2020-07-23 22:34:53 +0800

    fix again again

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 CURRNT_DIR=$(cd $(dirname "$0"); pwd)
-PUBLIC_FOLDER= "$CURRNT_DIR/../public"
+PUBLIC_FOLDER="$CURRNT_DIR/../public"
 
 aws s3 sync --region ap-northeast-1 --delete  $PUBLIC_FOLDER s3://sjmyuan-ganggang
